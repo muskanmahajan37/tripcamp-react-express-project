8859f5b7de47b4a3299e5388b5d839c0ddbf9e73
tony <tony@suas.com>
2020-12-18 08:55:34 -0500

    refactor profilebutton

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,60 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-export default function ProfileButton(){
+import * as sessionActions from '../../store/session';
+
+export default function ProfileButton({ user }) {
+  const [showMenu, setShowMenu] = useState(false);
+  const dispatch = useDispatch();
+
+  const dropdownMenuRef = useRef(null);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  function DropdownMenu() {
+    return (
+      <div className="dropdown-menu hidden" ref={dropdownMenuRef}>
+        <div className="dropdown-menu-item">{user.username}</div>
+        <hr className="hr" />
+        <div className="dropdown-menu-item">{user.email}</div>
+        <hr className="hr" />
+        <div className="dropdown-menu-item">
+          <button
+            className="button button-Reset"
+            onClick={logout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    );
+  }
+  function dropdownShowHide() {
+    if (showMenu) {
+      dropdownMenuRef.current.classList.remove("hidden");
+      dropdownMenuRef.current.classList.add("shown")
+    }
+    // else {
+    //   dropdownMenuRef.current.classList.remove("shown");
+    //   dropdownMenuRef.current.classList.add("hidden")
+    // }
+  }
+
+  useEffect(() => {
+    dropdownShowHide();
+  }, [showMenu])
+
   return (
-    <span className="fas fa-atom"></span>
+    <span
+      className="fas fa-atom"
+      onClick={() => {
+        setShowMenu(!showMenu);
+      }}
+    >
+      <DropdownMenu />
+    </span>
   );
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,4 +1,3 @@
-import { useState, useRef, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -6,39 +5,12 @@ import ProfileButton from './ProfileButton';
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
-  const [showMenu, setShowMenu] = useState(false);
-  const dropdownMenuRef = useRef(null);
 
-  function DropdownMenu() {
-    return (<div className="dropdown-menu hidden" ref={dropdownMenuRef} >
-      <div className="dropdown-menu-item">{sessionUser.username}</div>
-      <hr className="hr" />
-      <div className="dropdown-menu-item">{sessionUser.email}</div>
-      <hr className="hr" />
-      <div className="dropdown-menu-item">
-        <button className="button button-Reset">Logout</button>
-      </div>
-    </div>);
-  }
-
-  useEffect(() => {
-    if (showMenu) {
-      dropdownMenuRef.current.classList.remove("hidden");
-      dropdownMenuRef.current.classList.add("shown")
-    }
-  }, [showMenu])
   return (
     <nav>
       <NavLink to="/ ">Home</NavLink>
       { sessionUser ?
-        <>
-          <span onClick={() => {
-            setShowMenu(!showMenu);
-          }}>
-            <ProfileButton />
-          </span>
-          <DropdownMenu />
-        </>
+        <ProfileButton user={sessionUser}/>
         :
         <>
           <NavLink to="/login">Login</NavLink>
