f8024c761a11a699f7658b859e0f10a1c4ed0a9e
Tony Ngo <contact@suas.com>
2020-12-30 09:44:29 -0500

    add the first placeholder for search box at the middle of the banner

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -4,20 +4,21 @@ import { Link } from 'react-router-dom';
 import Spot, { AllSpots } from '../Spot';
 import UploadForm from '../UploadForm';
 import Banner from './Banner';
+import MainSearchBar from '../Search';
 
+import './Home.css';
 
 export default function Home() {
   return (
     <div className="main-home-view">
-      <Banner />
+      <div className='banner-and-search-div'>
+        <Banner />
+        <div className='search-over-banner-div main-search-bar'>
+          <MainSearchBar />
+        </div>
+      </div>
       <div className="main-home-view-spots" >
-        {/* <MapComponent /> */}
-        {/* <MapWithMarkerClusterer /> */}
-        {/* <Spot /> */}
         <AllSpots />
-        {/* <Link path='upload' > */}
-        {/* <UploadForm link="official"/> */}
-        {/* </Link> */}
       </div>
     </div>
   );
---
+++ frontend/src/components/Search/index.js
@@ -0,0 +1,11 @@
+
+import './Search.css';
+
+export default function MainSearchBar(){
+
+  return (
+    <div >
+      <input className='search-input' type='text' placeholder='Enter anything to search' />
+    </div>
+  );
+}
