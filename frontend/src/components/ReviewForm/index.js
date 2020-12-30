0200d0a6ece9ad4de2ddc3dd6dca2e4ec4d6dd8e
Tony Ngo <contact@suas.com>
2020-12-30 16:52:22 -0500

    fix the bug of a variable being one step behind the actual user's action

--- frontend/src/components/ReviewForm/index.js
+++ frontend/src/components/ReviewForm/index.js
@@ -12,7 +12,7 @@ import Rating from '../Rating';
 import '../Forms.css';
 import { nanoid } from 'nanoid';
 
-export default function ReviewFormModal({divClass = "modal", formContentClass = 'form-container modal-content'}) {
+export default function ReviewFormModal({ divClass = "modal", formContentClass = 'form-container modal-content' }) {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
   const spots = useSelector(state => state.spots);
@@ -23,13 +23,23 @@ export default function ReviewFormModal({divClass = "modal", formContentClass = 
   const reviewModalRef = useRef(null);
   const history = useHistory();
   const params = useParams();
-  const [trialRating, setTrialRating] = useState(null);
   const [rating, setRating] = useState(null);
+  let realtimeRating;
+  const [spot, setSpot] = useState(null);
 
-  // useEffect(() => {
-  //   setRating(ratings[ratings.length - 1]);
-  //   console.log('useEffect', rating);
-  // }, [ratings[ratings.length - 1]]);
+  useEffect(() => {
+    realtimeRating = ratings[ratings.length - 1];
+    setRating(realtimeRating);
+    console.log('realtimeRating', realtimeRating);
+  }, [ratings]);
+
+  useEffect(() => {
+    if (params && spots) {
+      setSpot(spots.find(spot => spot.id === Number(params.spotId)));
+      realtimeRating = undefined;
+    }
+    console.log("spot", spot, params, realtimeRating);
+  }, [params]);
 
   if (!sessionUser) {
     if (reviewModalRef.current)
@@ -38,27 +48,11 @@ export default function ReviewFormModal({divClass = "modal", formContentClass = 
     return <Redirect to='/login' />;
   }
 
-  let spot;
-  let spotInfo = <></>;
-
-  //TODO: make this useEffect work so it won't reload spot data everytime user types a key
-  // useEffect(() => {
-  if (params && spots) spot = spots.find(spot => spot.id === Number(params.spotId));
-  // console.log("spot", spot, params);
-  spotInfo = spot && <>
-    <p>Spot:</p>
-    <p>{spot.name}</p>
-    <p>{spot.streetAddress}</p>
-  </>
-  // });
-
   const handleSubmit = e => {
     e.preventDefault();
     setErrors([]);
 
-    // console.log('ratings', ratings);
-    // setRating(ratings[ratings.length-1]);    
-    if (!rating)
+    if (!realtimeRating)
       return setErrors(["Select a rating"]);
 
     return dispatch(reviewActions.createOneReview({
@@ -67,7 +61,7 @@ export default function ReviewFormModal({divClass = "modal", formContentClass = 
         spotId: spot.id,
         title,
         body,
-        rating,
+        rating: realtimeRating
       }
     }))
       .then(res => {
@@ -87,27 +81,6 @@ export default function ReviewFormModal({divClass = "modal", formContentClass = 
     history.push('/');
   }
 
-  // const handleFormClick = e => {
-  //   if (e.target.id[1] === '-')
-  //     setRating(ratings[ratings.length - 1])
-  //   console.log('handleFormClick ratings', ratings);
-  // };
-
-  let arrayOf5 = new Array(5).fill(1);
-
-  function onMouseOver(e) {
-    const id = e.target.id.split("-")[0];
-    setTrialRating(Number(id));
-  }
-  function onMouseLeave() {
-    setTrialRating(rating);
-  }
-  function onStarClicked(e) {
-    const ratingNumber = Number(e.target.id.split("-")[0]);
-    setRating(ratingNumber);
-    setTrialRating(ratingNumber)
-  }
-
   return (
     <div className={divClass} ref={reviewModalRef}>
       <form
@@ -118,7 +91,11 @@ export default function ReviewFormModal({divClass = "modal", formContentClass = 
         <h3>Review Form</h3>
         <div>
           {
-            spotInfo
+            spot && <>
+              <p>Spot:</p>
+              <p>{spot.name}</p>
+              <p>{spot.streetAddress}</p>
+            </>
           }
         </div>
         <ul className='error-messages'>
@@ -149,18 +126,7 @@ export default function ReviewFormModal({divClass = "modal", formContentClass = 
           </div>
           <div className="input-div">
             <label>Rating</label>
-            <div onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>
-              <span id={0 + "-" + nanoid()} onClick={onStarClicked} className="far fa-star star-set-font-size" style={{ color: "rgba(0,0,0,0)" }}> </span>
-              {arrayOf5.map((el, i) => <span
-                className={i < trialRating ? "fa fa-star checked star-set-font-size" : "far fa-star star-set-font-size"}
-                key={nanoid()}
-                id={(i + 1) + "-" + nanoid()}
-                onClick={onStarClicked}
-              />
-              )}
-              <span className="far fa-star star-set-font-size" style={{ color: "rgba(0,0,0,0)" }}>{ }</span>
-            </div>
-            {/* <Rating userChangeable={true} id={"rating" + nanoid()} /> */}
+            <Rating userChangeable={true} id={"rating" + nanoid()} />
           </div>
         </div>
         <div className="buttons-div">
