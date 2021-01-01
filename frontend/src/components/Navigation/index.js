a431feb567d0a60e0b3081478b499e8732313edb
Tony Ngo <contact@suas.com>
2020-12-31 19:39:02 -0500

    add dispatch to navlink home button to remove that currentSpot to fix the blank screen bug

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,16 +1,26 @@
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { NavLink, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ProfileButton from './ProfileButton';
 import NotificationBell from './NotificationBell';
 import GithubLink from './GithubLink';
+import { removeCurrentSpot } from '../../store/spot';
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   return (
     <nav className="main-navbar">
-      <NavLink to={'/'}>Home</NavLink>
+      <NavLink to={'/'} onClick={e => {
+        e.preventDefault();
+        dispatch(removeCurrentSpot());
+        console.log('removeCurrentSpot');
+        history.push('/');
+      }}>
+        Home
+      </NavLink>
       { sessionUser ?
         <>
           <NotificationBell user={sessionUser} />
